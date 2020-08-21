0557a42c4c9ef481c53943ddb8345bcd7fc408f2
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-08-21 23:52:15 +0300

    task4-6

--- main.js
+++ main.js
@@ -22,8 +22,7 @@ function myPow(a, b, callback) {
   let pow = (x, n) => {
     if (n !== 1) {
       return x *= pow(x, n - 1);
-    }
-    else {
+    } else {
       return x;
     }
   };
@@ -35,3 +34,49 @@ function myPrint(a, b, res) {
 }
 
 console.log(myPow(3, 4, myPrint));
+
+//task 4
+function info() {
+  return `${this.model} ${this.name}, ${this.year}cc, year ${this.year}, ${this.used}`;
+}
+let yearNow = new Date().getFullYear();
+let car = {
+  engine: 2000,
+  model: 'Chevrolet',
+  name: 'Lacetti',
+  year: 2010,
+  get used() {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) {
+      this.year = yearNow;
+    }
+  },
+  info: info
+};
+
+let car1 = {
+  engine: 5000,
+  model: 'infinity',
+  name: 'FX50 AWD',
+  year: 2020,
+  get used() {
+    return this.year !== yearNow ? 'used' : 'new';
+  },
+  set used(value) {
+    if (value === 'new' && this.year < yearNow) {
+      this.year = yearNow;
+    }
+  },
+  info: info
+};
+
+console.log(car.info());
+car.used = 'new';
+console.log(car.info());
+car.used = 'used';
+console.log(car.info());
+console.log(car1.info());
+car.used = 'used';
+console.log(car1.info());
