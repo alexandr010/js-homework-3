ca401484d2f164f968019758f9c82c405da4f3c9
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-08-23 09:23:50 +0300

    task done

--- main.js
+++ main.js
@@ -12,38 +12,59 @@ console.log(counter());
 console.log(counter(10));
 
 //task 2
-let counting = function () {
-  let num = 0;
-
-};
-
-//task 3
-function myPow(a, b, callback) {
-  let pow = (x, n) => {
-    if (n !== 1) {
-      return x *= pow(x, n - 1);
-    } else {
-      return x;
+let counting = (function () {
+  let count = 0;
+  return {
+    value(num) {
+      if (num !== undefined) count = num;
+      return count;
+    },
+    decrement() {
+      count--;
+    },
+    increment() {
+      count++;
     }
   };
-  return callback(a, b, pow(a, b));
-}
+}());
 
-function myPrint(a, b, res) {
-  return `${a} ^ ${b} = ${res}`;
-}
+console.log(counting.value()); // 0
+counting.increment();
+counting.increment();
+counting.increment();
+console.log(counting.value()); // 3
+counting.decrement();
+counting.decrement();
+console.log(counting.value()); // 1
+console.log(counting.value(100)); // 100
+counting.decrement();
+console.log(counting.value()); // 99
+console.log(counting.value(200)); // 200
+counting.increment();
+console.log(counting.value()); // 201
+
+//task 3
+let myPow = (a, b, callback) => {
+  let pow = (x, n) => {
+    if (n !== 1) {return x *= pow(x, n - 1);}
+      return x;
+    };
+  return callback(a, b, pow(a, b));
+};
+
+let myPrint = (a, b, res) => `${a} ^ ${b} = ${res}`;
 
 console.log(myPow(3, 4, myPrint));
 
-//task 4
+//task 4 - 6
 function info() {
-  return `${this.model} ${this.name}, ${this.year}cc, year ${this.year}, ${this.used}`;
+  return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
 }
 let yearNow = new Date().getFullYear();
 let car = {
   engine: 2000,
-  model: 'Chevrolet',
-  name: 'Lacetti',
+  model: 'Lacetti',
+  name: 'Chevrolet',
   year: 2010,
   get used() {
     return this.year !== yearNow ? 'used' : 'new';
@@ -58,8 +79,8 @@ let car = {
 
 let car1 = {
   engine: 5000,
-  model: 'infinity',
-  name: 'FX50 AWD',
+  model: 'FX50 AWD',
+  name: 'infinity',
   year: 2020,
   get used() {
     return this.year !== yearNow ? 'used' : 'new';
@@ -80,3 +101,39 @@ console.log(car.info());
 console.log(car1.info());
 car.used = 'used';
 console.log(car1.info());
+
+//task 7
+let list = [12, 23, 100, 34, 56, 9, 233];
+let myMax = (arr) => Math.max.apply(Math, arr);
+
+
+console.log(myMax(list)); // 233
+
+//task 8
+
+function myMul(a, b) {
+  return a * b;
+}
+let myDouble = myMul.bind(null, 2);
+let myTriple = myMul.bind(null, 3);
+
+console.log(myDouble(3)); // = myMul(2, 3) = 6
+console.log(myTriple(3)); // = myMul(3, 3) = 9
+
+
+//task 9
+function myUniq(arr) {
+  let set = new Set(arr);
+  // arr.forEach((value) => {
+  //   set.add(value);
+  // });
+  return set;
+}
+
+let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
+
+let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
+
+console.log(myUniq(notUniqNums));
+
+console.log(myUniq(notUniqStrings));
